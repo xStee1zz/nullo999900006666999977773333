2376d792eb93d302a93084195a73f63c00570410
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2024-12-20 22:07:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ async def handler(message: types.Message) -> None:
               }
             }
 
-            async with session.post(f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=AIzaSyBhBmYWyhvewEG5Gf7gCHHzmdsmFbMm22A", json=data) as response:
+            async with session.post(f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=AIzaSyBbP1TPnlKgFtPAcAEqVuWvyjFtEnTEMho", json=data) as response:
                 try:
                     await message.reply((await response.json())["candidates"][0]["content"]["parts"][0]["text"], parse_mode="Markdown")
                 except Exception as e:
