18e479d5f2656b2e570bc8511ee934cce402990e
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2024-12-21 01:41:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ dp = Dispatcher()
 
 @dp.message(F.chat.id == -1002258024710, F.text)
 async def handler(message: types.Message) -> None:
-    if message.text.lower() == "мут":
+    if message.text.lower().startswith("мут"):
         if (await bot.get_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)).status in ["administrator", "creator"]:
             if message.reply_to_message:
                 if (await bot.get_chat_member(chat_id=message.chat.id, user_id=message.reply_to_message.from_user.id)).status in ["administrator", "creator"]:
