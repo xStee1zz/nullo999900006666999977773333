7feed4a303443bed8d8f666b816cc3b157b17138
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2024-12-22 00:19:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,9 @@ async def handler(message: types.Message) -> None:
                         kolvo = text[3] + text[4]
                         if int(kolvo) in (10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60):
                             end_time = datetime.now(pytz.timezone("Europe/Moscow")) + timedelta(minutes=int(kolvo))
-                            
+                else:
+                    await message.reply("Неверный формат.\n\nДопустимый формат:\n - мут <1-3>д\n - мут <1-24>ч\n - мут <1-60>м")
+                    return
 
                 end_time_timestamp = int(end_time.timestamp())
                 formatted_end_time = f"{end_time.day} {months[end_time.strftime('%B')]} {end_time.hour}:{end_time.strftime('%M')}"
