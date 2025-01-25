2caee7cb9ae3cb33c2aa494a9bf5b4772a1203d8
xStee1zz <164562517+xStee1zz@users.noreply.github.com>
2025-01-25 21:44:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ async def handler(message: types.Message):
         return
 
     elif message.text.startswith("."):
-        text = message.text[1:]
+        text = message.text[1:].lstrip()
         if not text:
             return
 
